d3d9d7aeeb6ba7f2bc952a8e040dc64db9b11a16
Chris Bell <chrisbell.dev@gmail.com>
2020-10-05 22:02:10 -0700

    Move index.html into public folder

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const port = 3000;
 
 app.use(express.static(__dirname + '/../client/dist/'));
+app.use(express.static(__dirname + '/../client/public/'));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
