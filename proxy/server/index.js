07bd531858c27848fd73bf4c7b3baf28fac29433
Chris Bell <chrisbell.dev@gmail.com>
2020-10-07 10:43:54 -0700

    Setup proxies to services

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -5,12 +5,17 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = 3000;
 
 app.use(express.static(__dirname + '/../client/public/'));
-app.use('/stays/:id', function (req, res, next) {
-  console.log('Request Type:', req.method)
-  next();
-});
 
-app.use('/stays/:id', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true }));
+// proxy to gallery api
+app.use('/gallery/', createProxyMiddleware({target: 'http://localhost:3001', changeOrigin: true }));
+// proxy to booking api
+app.use('/booking/', createProxyMiddleware({target: 'http://localhost:3002', changeOrigin: true }));
+// proxy to review api
+app.use('/reviews/', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true }));
+// proxy to review assets
+app.use('/reviews-static', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true }));
+// proxy to more-places api
+app.use('/more-places/', createProxyMiddleware({target: 'http://localhost:3004', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
