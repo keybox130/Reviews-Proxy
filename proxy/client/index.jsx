99c414c8d24b4e80cb1c5d0e355a6a68c7371553
Chris Bell <chrisbell.dev@gmail.com>
2020-10-06 20:11:25 -0700

    Redirect stay api requests to review backend

--- proxy/client/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-window.onload = () => {
-  ReactDOM.render(<GalleryApp />, document.getElementById('gallery'));
-  ReactDOM.render(<BookingApp />, document.getElementById('booking'));
-  ReactDOM.render(<ReviewApp />, document.getElementById('reviews'));
-  ReactDOM.render(<MorePlacesApp />, document.getElementById('more-places-to-stay'));
-}
--- proxy/server/index.js
+++ proxy/server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = 3000;
 
-app.use(express.static(__dirname + '/../client/dist/'));
 app.use(express.static(__dirname + '/../client/public/'));
+app.use('/stays/:id', function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next();
+});
+
+app.use('/stays/:id', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
