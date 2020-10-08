53ed628bd2b3de8f849901e34c9c4d162efe55ac
Chris Bell <chrisbell.dev@gmail.com>
2020-10-07 17:22:55 -0700

    Redirect root to stay id of 1

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -4,18 +4,25 @@ const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = 3000;
 
-app.use(express.static(__dirname + '/../client/public/'));
+app.get('/', (req, res) => {
+  res.redirect('stay/1');
+});
+
+app.use('/stay/:id', (req, res, next) => {
+  const { id } = req.params;
+  express.static(__dirname + '/../client/public/')(req, res, next);
+});
 
 // proxy to gallery api
-app.use('/gallery/', createProxyMiddleware({target: 'http://localhost:3001', changeOrigin: true }));
-// proxy to booking api
-app.use('/booking/', createProxyMiddleware({target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/gallery/stays/:id', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 // proxy to review api
-app.use('/reviews/', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/reviews/stays/:id', createProxyMiddleware({ target: `http://localhost:3003`, changeOrigin: true }));
 // proxy to review assets
-app.use('/reviews-static', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/reviews-static', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+// proxy to booking api
+app.use('/booking/', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
 // proxy to more-places api
-app.use('/more-places/', createProxyMiddleware({target: 'http://localhost:3004', changeOrigin: true }));
+app.use('/more-places/', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
